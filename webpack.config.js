9e155b15c70c3261e919fb5c6b19d65c23e80442
Roberto Rossetti <roberto.rossetti@unicaspa.it>
2023-03-25 19:41:34 +0100

    cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     entry: './src/index.ts',
@@ -41,5 +41,6 @@ module.exports = {
             filename: "[name].css",
             chunkFilename: "[id].css",
         }),
+        new CleanWebpackPlugin(),
     ]
 }
